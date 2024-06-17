80a1d86c34889c876be8460d3588ca8fcc4cfd04
박지영 <barkji0@naver.com>
2024-06-17 22:15:29 +0900

    React Router Dom 적용하기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import LandingPage from './pages/LandingPage'
+import RegisterPage from './pages/RegisterPage'
+import Navbar from './layout/NavBar'
+import Footer from './layout/Footer'
+
+function Layout() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold underline'>
-        Hello World!
-      </h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {/* 모두 */}
+        <Route index element={<LandingPage />} />
+        
+        {/* 로그인 안한 사람 */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   )
 }
 
