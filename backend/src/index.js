ee8355a4a5773347cae0abaef353bb327f36bc45
박지영 <barkji0@naver.com>
2024-06-24 00:05:43 +0900

    backend 기본 구조 생성

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+// Node.js의 진입점
+
+const express = require('express');
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('안녕하세요!');
+})
+
+app.listen(port, () => {
+  console.log(`${port}번에서 실행이 되었습니다.`);
+});
