6d6c99b4632447b28b5424ac8a8d93fe226da3da
박지영 <barkji0@naver.com>
2024-06-24 00:17:06 +0900

    express.static()

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,15 @@
 // Node.js의 진입점
 
 const express = require('express');
-
+const path = require('path'); // 절대 경로 사용 위해
 const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
   res.send('안녕하세요!');
 })
+
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 app.listen(port, () => {
   console.log(`${port}번에서 실행이 되었습니다.`);
