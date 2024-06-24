18d41eacedabbf000eab920e8f8cbf5d33c23b0e
박지영 <barkji0@naver.com>
2024-06-25 00:10:00 +0900

    mongoDB 연결

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,20 @@ const path = require('path'); // 절대 경로 사용 위해
 const cors = require('cors');
 const app = express();
 const port = 4000;
+const mongoose = require('mongoose'); // mongoDB 사용 위함
+const dotenv = require('dotenv'); // 환경변수 사용 위함
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('mongodb 연결 완료')
+  })
+  .catch(err => {
+    console.error(err);
+  })
 
 app.get('/', (req, res) => {
   res.send('안녕하세요!');
