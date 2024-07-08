eb2bf5f0c237571984f35751354db251addaedae
박지영 <barkji0@naver.com>
2024-07-08 19:44:03 +0900

    가입 시 비밀번호 암호화 적용(bcryptjs)

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,13 +2,14 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
   // 유저 데이터 저장
   try {
     const user = new User(req.body);
     await user.save();
     return res.sendStatus(200);
   } catch (error) {
+    console.log(error);
     next(error);
   }
 })
