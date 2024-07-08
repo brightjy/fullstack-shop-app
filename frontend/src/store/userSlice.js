db7d8bfd4f651a3168a23d3ab9ef8c2665925f53
박지영 <barkji0@naver.com>
2024-07-08 20:38:23 +0900

    로그인 페이지 생성

--- frontend/src/store/userSlice.js
+++ frontend/src/store/userSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit"
 import { registerUser } from "./thunkFunctions";
+import { loginUser } from "./thunkFunctions";
 import { toast } from "react-toastify";
 
 const initialState = {
@@ -32,7 +33,21 @@ const userSlice = createSlice({
         state.isLoading = false;
         state.error = action.payload;
         toast.error(action.payload);
-    })
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.userData = action.payload;
+        state.isAuth = true;
+        localStorage.setItem('accessToken', action.payload.accessToken);
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+        toast.error(action.payload);
+      })
   }
 })
 
