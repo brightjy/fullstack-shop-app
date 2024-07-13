6d23e6898898ae43a201f33d433fcea766b9a815
박지영 <barkji0@naver.com>
2024-07-13 19:40:04 +0900

    상품 관련 page, model 생성

---
+++ backend/src/models/Product.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const productSchema = mongoose.Schema({
+  writer: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  title: {
+    type: String,
+    maxLength: 30,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  images: {
+    type: Array,
+    default: [],
+  },
+  sold: {
+    type: Number,
+    maxLength: 100,
+    default: 0,
+  },
+  continents: {
+    type: Number,
+    default: 1,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  }
+})
+
+const Product = mongoose.model("Product", productSchema);
+
+model.exports = Product;
