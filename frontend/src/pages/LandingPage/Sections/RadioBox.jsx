b64caccaae845f02579232a679ec03faa0c060bc
박지영 <barkji0@naver.com>
2024-07-24 21:52:15 +0900

    라디오버튼 필터 구현

--- frontend/src/pages/LandingPage/Sections/RadioBox.jsx
+++ frontend/src/pages/LandingPage/Sections/RadioBox.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const RadioBox = () => {
+const RadioBox = ({ prices, checkedPrice, onFilters }) => {
   return (
-    <div>RadioBox</div>
+    <div className='p-2 mb-3 bg-gray-100 rounded-md text-black'>
+      {prices?.map(price => (
+        <div key={price._id}>
+          <input
+            type='radio'
+            checked={checkedPrice === price.array}
+            onChange={e => onFilters(e.target.value)}
+            id={price._id}
+            value={price._id}
+          />
+          {" "}
+          <label htmlFor={price._id}>{price.name}</label>
+        </div>
+      ))}
+    </div>
   )
 }
 
