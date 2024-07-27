a112466b2ff6207113bbae3f4718f0d1e96ea8e5
박지영 <barkji0@naver.com>
2024-07-27 15:18:31 +0900

    title like 검색 되게 수정

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -49,7 +49,7 @@ router.get('/', async (req, res, next) => {
   }
 
   if (term) {
-    findArgs["$text"] = { $search: term };
+    findArgs["title"] = { $regex: term };
   }
 
   console.log(findArgs);
