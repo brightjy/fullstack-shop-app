848098c6cca8cda4dda737d2fb1e62f68d680936
박지영 <barkji0@naver.com>
2024-08-28 21:57:14 +0900

    product Info 추가

--- frontend/src/pages/DetailProductPage/Sections/ProductInfo.jsx
+++ frontend/src/pages/DetailProductPage/Sections/ProductInfo.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../../store/thunkFunctions';
 
-const ProductInfo = () => {
+const ProductInfo = ({ product }) => {
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(addToCart({ productId: product._id}));
+  }
+
   return (
-    <div>ProductInfo</div>
+    <div>
+      <p className='text-xl text-bold'>상품 정보</p>
+
+      <ul>
+        <li><span className='font-semibold'>가격: </span> {product.price} 원</li>
+        <li><span className='font-semibold'>팔린 개수: </span> {product.sold} 개</li>
+        <li><span className='font-semibold'>설명: </span> {product.description}</li>
+      </ul>
+
+      <div className='mt-3'>
+        <button 
+          onClick={ handleClick }
+          className='w-full px-4 py-2 bg-black text-white hover:bg-gray-700 rounded-md'>
+          장바구니로
+        </button>
+      </div>
+    </div>
   )
 }
 
